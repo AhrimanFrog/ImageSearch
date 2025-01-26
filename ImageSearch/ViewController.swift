3f0a7e1dbe555995779a4e94eb1e3004e13616ad
Karpachov Illia <karpachov.i@ajax.systems>
2025-01-26 23:35:13 +0200

    Initial Commit

---
+++ ImageSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageSearch
+//
+//  Created by petro glyst on 26.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ImageSearchTests/ImageSearchTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageSearchTests.swift
+//  ImageSearchTests
+//
+//  Created by petro glyst on 26.01.2025.
+//
+
+import Testing
+@testable import ImageSearch
+
+struct ImageSearchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
